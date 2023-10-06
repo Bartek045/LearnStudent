7799a5e3285c45e718965290c270d817c9353374
Bartek Kowalczyk <barteknock@outlook.com>
2023-10-06 21:25:23 +0200

    Initial commit

---
+++ LearnS.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,56 @@
+﻿using LearnS.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearnS.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Math", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Automatics", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Physics", DisplayOrder = 3 }
+                );
+            modelBuilder.Entity<Section>().HasData(
+                new Section
+                {
+                    Id = 1,
+                    Title = "Statystyka",
+                    Description = "Statystyka opisowa",
+                    Author = "eTrapez",
+                    CategoryId = 1
+                },
+                  new Section
+                  {
+                      Id = 2,
+                      Title = "Statystyka",
+                      Description = "Przedziały ufności. Estymacja przedziałowe.",
+                      Author = "eTrapez",
+                      CategoryId = 1
+                  },
+                    new Section
+                    {
+                        Id = 3,
+                        Title = "Statystyka",
+                        Description = "Parametryczne testy istotności",
+                        Author = "eTrapez",
+                        CategoryId = 1
+                    }
+
+
+                );
+        }
+    }
+}
---
+++ LearnS.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using LearnS.DataAccess.Data;
+using LearnS.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnS.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+        public ICategoryRepository Category { get; private set; }
+        public ISectionRepository Section { get; private set; }
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            Section = new SectionRepository(_db);
+        }
+        
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
