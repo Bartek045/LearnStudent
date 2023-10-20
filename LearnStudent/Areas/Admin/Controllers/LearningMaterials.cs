52c06c908dbf88885a24630657ba9fcf4b9decf2
Bartek Kowalczyk <barteknock@outlook.com>
2023-10-20 22:50:08 +0200

    add db to quiz and materials

--- LearnStudent/Areas/Admin/Controllers/LearningMaterials.cs
+++ LearnStudent/Areas/Admin/Controllers/LearningMaterials.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LearnS.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LearnStudent.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
     public class LearningMaterials : Controller
     {
         public IActionResult Index()
