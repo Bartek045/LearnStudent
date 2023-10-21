48361caa6df214c19a354b1edb1efd848aebfbbc
Bartek Kowalczyk <barteknock@outlook.com>
2023-10-21 19:21:47 +0200

    Display materials don't working yet

---
+++ LearnStudent/Areas/User/Controllers/LearningMaterialsController.cs
@@ -0,0 +1,17 @@
+﻿using LearnS.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnStudent.Areas.User.Controllers
+{
+    [Area("User")]
+
+
+    public class LearningMaterialsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
