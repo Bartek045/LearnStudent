2042bd026dab7cdae15792ca19f342960e868ce5
Knieciu <gabennewell00@gmail.com>
2023-11-02 15:34:48 +0100

    fix

--- LearnStudent/wwwroot/js/site.js
+++ LearnStudent/wwwroot/js/site.js
@@ -11,22 +11,3 @@ function closeNav() {
 }
 
 
-document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
-    const dropDownBtn = dropDownWrapper.querySelector('.dropdown .levels');
-    const dropDownContent = dropDownWrapper.querySelector('.dropdown-content');
-
-    dropDownBtn.addEventListener('click', function (e) {
-        e.preventDefault();
-        dropDownContent.classList.toggle('show');
-    });
-});
-
-document.querySelectorAll('.dropdown2').forEach(function (dropDownWrapper) {
-    const dropDownBtn = dropDownWrapper.querySelector('.dropdown2 .levels');
-    const dropDownContent = dropDownWrapper.querySelector('.dropdown-content2');
-
-    dropDownBtn.addEventListener('click', function (e) {
-        e.preventDefault();
-        dropDownContent.classList.toggle('show');
-    });
-});
