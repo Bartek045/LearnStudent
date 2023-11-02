f77ac269253d9cffed5bba3759856ebaf9e0070d
Knieciu <gabennewell00@gmail.com>
2023-11-02 13:10:45 +0100

    View for avatar and delete second navbar toggle

--- LearnStudent/wwwroot/js/site.js
+++ LearnStudent/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿function openNav() {
+﻿
+
+function openNav() {
+    
     document.getElementById("mySidebar").style.width = "250px";
 
 }
@@ -6,3 +9,24 @@
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
 }
+
+
+document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
+    const dropDownBtn = dropDownWrapper.querySelector('.dropdown .levels');
+    const dropDownContent = dropDownWrapper.querySelector('.dropdown-content');
+
+    dropDownBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+        dropDownContent.classList.toggle('show');
+    });
+});
+
+document.querySelectorAll('.dropdown2').forEach(function (dropDownWrapper) {
+    const dropDownBtn = dropDownWrapper.querySelector('.dropdown2 .levels');
+    const dropDownContent = dropDownWrapper.querySelector('.dropdown-content2');
+
+    dropDownBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+        dropDownContent.classList.toggle('show');
+    });
+});
