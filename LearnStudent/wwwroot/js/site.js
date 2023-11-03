f96778cee52b2dd4b6f0b3d229abc1e729da6340
Knieciu <gabennewell00@gmail.com>
2023-11-03 14:33:16 +0100

    sidebar onClick

--- LearnStudent/wwwroot/js/site.js
+++ LearnStudent/wwwroot/js/site.js
@@ -10,4 +10,113 @@ function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
 }
 
+function showContent1() {
+    
+    
+
+    if (document.getElementById("dropdown-content1").style.display == "block") {
+        document.getElementById("dropdown-content1").style.display = "none";
+    }
+    else {
+        document.getElementById("dropdown-content1").style.display = "block";
+    }
+}
+
+function showContent11() {
+
+
+
+    if (document.getElementById("dropdown-content11").style.display == "block") {
+        document.getElementById("dropdown-content11").style.display = "none";
+    }
+    else {
+        document.getElementById("dropdown-content11").style.display = "block";
+    }
+}
+
+function showContent12() {
+
+
+
+    if (document.getElementById("dropdown-content12").style.display == "block") {
+        document.getElementById("dropdown-content12").style.display = "none";
+    }
+    else {
+        document.getElementById("dropdown-content12").style.display = "block";
+    }
+}
+
+
+function showContent2() {
+
+
+
+    if (document.getElementById("dropdown-content2").style.display == "block") {
+        document.getElementById("dropdown-content2").style.display = "none";
+    }
+    else {
+        document.getElementById("dropdown-content2").style.display = "block";
+    }
+}
+
+function showContent21() {
+
+
+
+    if (document.getElementById("dropdown-content21").style.display == "block") {
+        document.getElementById("dropdown-content21").style.display = "none";
+    }
+    else {
+        document.getElementById("dropdown-content21").style.display = "block";
+    }
+}
+
+function showContent22() {
+
+
+
+    if (document.getElementById("dropdown-content22").style.display == "block") {
+        document.getElementById("dropdown-content22").style.display = "none";
+    }
+    else {
+        document.getElementById("dropdown-content22").style.display = "block";
+    }
+}
+
+
+function showContent3() {
+
+
+
+    if (document.getElementById("dropdown-content3").style.display == "block") {
+        document.getElementById("dropdown-content3").style.display = "none";
+    }
+    else {
+        document.getElementById("dropdown-content3").style.display = "block";
+    }
+}
+
+function showContent31() {
+
+
+
+    if (document.getElementById("dropdown-content31").style.display == "block") {
+        document.getElementById("dropdown-content31").style.display = "none";
+    }
+    else {
+        document.getElementById("dropdown-content31").style.display = "block";
+    }
+}
+
+function showContent32() {
+
+
+
+    if (document.getElementById("dropdown-content32").style.display == "block") {
+        document.getElementById("dropdown-content32").style.display = "none";
+    }
+    else {
+        document.getElementById("dropdown-content32").style.display = "block";
+    }
+}
 
